6be3c1184232f14c9a1813446b056af302278e0a
tariqwalji <tariqwalji@yahoo.co.uk>
2019-10-29 22:45:27 +0000

    Check reply with mention can be read by original poster

--- src/Listener/UpdateMentionsMetadataWhenVisible.php
+++ src/Listener/UpdateMentionsMetadataWhenVisible.php
@@ -76,7 +76,7 @@ class UpdateMentionsMetadataWhenVisible
             ->whereIn('id', $mentioned)
             ->get()
             ->filter(function ($post) use ($reply) {
-                return $post->user && $post->user->id !== $reply->user_id;
+                return $post->user && $post->user->id !== $reply->user_id && $reply->isVisibleTo($post->user);
             })
             ->all();
 
