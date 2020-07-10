4b7cf29928840cb21720f59133c83775ba195bcb
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-07-10 17:33:58 -0400

    More StyleCI

--- migrations/2020_03_03_060845_combine_mentions.php
+++ migrations/2020_03_03_060845_combine_mentions.php
@@ -5,7 +5,7 @@
  *
  * For detailed copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
-*/
+ */
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
