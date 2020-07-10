0c9f98704009d0b4f2b11d511ddeff31fce04e7e
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-07-10 17:31:27 -0400

    Use raw sql for consolidation query, replace primary key, fix typo

--- migrations/2020_03_03_060845_combine_mentions.php
+++ migrations/2020_03_03_060845_combine_mentions.php
@@ -8,44 +8,24 @@ return [
     'up' => function (Builder $schema) {
         // Create new table
         $schema->create('post_mentions', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('post_id')->unsigned();
             $table->integer('mentions_post_id')->unsigned()->nullable();
             $table->integer('mentions_user_id')->unsigned()->nullable();
-            $table->primary(['post_id']);
         });
 
         // Establish foreignkey constraints
         $schema->table('post_mentions', function (Blueprint $table) {
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-            $table->foreign('mentions_user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('mentions_post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('mentions_user_id')->references('id')->on('users')->onDelete('cascade');
         });
 
-        // Move stuff from post mentions table
-        $postMentions = $schema->getConnection()->table('post_mentions_post')->cursor();
-
-        foreach($postMentions as $postMention) {
-            $schema
-                ->getConnection()->table('post_mentions')
-                ->insert([
-                    'post_id' => $postMention->post_id,
-                    'mentions_post_id' => $postMention->mentions_post_id,
-                    'mentions_user_id' => null
-                ]);
-        }
-
-        // Move stuff from user mentions table
-        $userMentions = $schema->getConnection()->table('post_mentions_user')->cursor();
-
-        foreach ($userMentions as $userMention) {
-            $schema
-                ->getConnection()->table('post_mentions')
-                ->insert([
-                    'post_id' => $userMention->post_id,
-                    'mentions_post_id' => null,
-                    'mentions_user_id' => $userMention->mentions_user_id
-                ]);
-        }
+        $prefix = $schema->getConnection()->getTablePrefix();
+        $schema->getConnection()->statement("INSERT INTO `${prefix}post_mentions` (`post_id`, `mentions_post_id`, `mentions_user_id`)
+            SELECT `post_id`, `mentions_post_id`, NULL AS `mentions_user_id` FROM `${prefix}post_mentions_post`
+            UNION ALL
+            SELECT `post_id`, NULL AS `mentions_post_id`, `mentions_user_id` FROM `${prefix}post_mentions_user`;");
 
         // Drop old tables
         $schema->dropIfExists('post_mentions_post');
@@ -54,19 +34,23 @@ return [
 
     'down' => function (Builder $schema) {
         // Recreate old tables
-        $schema->create('post_mentions_users',
+        $schema->create(
+            'post_mentions_user',
             function (Blueprint $table) {
                 $table->integer('post_id')->unsigned();
                 $table->integer('mentions_post_id')->unsigned();
                 $table->primary(['post_id', 'mentions_post_id']);
-        });
+            }
+        );
 
-        $schema->create('post_mentions_post',
+        $schema->create(
+            'post_mentions_post',
             function (Blueprint $table) {
                 $table->integer('post_id')->unsigned();
                 $table->integer('mentions_post_id')->unsigned();
                 $table->primary(['post_id', 'mentions_post_id']);
-        });
+            }
+        );
 
         // Recreate old foreign keys
         $schema->table('post_mentions_post', function (Blueprint $table) {
@@ -79,26 +63,10 @@ return [
             $table->foreign('mentions_user_id')->references('id')->on('users')->onDelete('cascade');
         });
 
-        // Move stuff from new table to old table
-        $mentions = $schema->getConnection()->table('post_mentions')->cursor();
+        $prefix = $schema->getConnection()->getTablePrefix();
+        $schema->getConnection()->statement("INSERT INTO `${prefix}post_mentions_post` SELECT * FROM `${prefix}post_mentions` WHERE `mentions_post_id` IS NOT NULL;");
 
-        foreach ($mentions as $mention) {
-            if ($mention->mentions_user_id) {
-                $schema
-                    ->getConnection()->table('post_mentions_user')
-                    ->insert([
-                        'post_id' => $mention->post_id,
-                        'mentions_user_id' => $mention->mentions_user_id
-                    ]);
-            } else if ($mention->mentions_post_id) {
-                $schema
-                    ->getConnection()->table('post_mentions_post')
-                    ->insert([
-                        'post_id' => $mention->post_id,
-                        'mentions_post_id' => $mention->mentions_post_id
-                    ]);
-            }
-        }
+        $schema->getConnection()->statement("INSERT INTO `${prefix}post_mentions_user` SELECT * FROM `${prefix}post_mentions` WHERE `mentions_user_id` IS NOT NULL;");
 
         // Drop new table
         $schema->dropIfExists('post_mentions');
