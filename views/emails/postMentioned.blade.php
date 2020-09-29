c75896b9ceef3ffa71176882aefdb470bdefa862
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-09-28 20:56:04 -0400

    Call variables display_name instead of username

--- views/emails/postMentioned.blade.php
+++ views/emails/postMentioned.blade.php
@@ -1,6 +1,6 @@
 {!! $translator->trans('flarum-mentions.email.post_mentioned.body', [
-'{recipient_username}' => $user->display_name,
-'{replier_username}' => $blueprint->post->user->display_name,
+'{recipient_display_name}' => $user->display_name,
+'{replier_display_name}' => $blueprint->post->user->display_name,
 '{post_number}' => $blueprint->post->number,
 '{title}' => $blueprint->post->discussion->title,
 '{url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->reply->discussion_id, 'near' => $blueprint->reply->number]),
--- views/emails/userMentioned.blade.php
+++ views/emails/userMentioned.blade.php
@@ -1,6 +1,6 @@
 {!! $translator->trans('flarum-mentions.email.user_mentioned.body', [
-'{recipient_username}' => $user->display_name,
-'{mentioner_username}' => $blueprint->post->user->display_name,
+'{recipient_display_name}' => $user->display_name,
+'{mentioner_display_name}' => $blueprint->post->user->display_name,
 '{title}' => $blueprint->post->discussion->title,
 '{url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->post->discussion_id, 'near' => $blueprint->post->number]),
 '{content}' => $blueprint->post->content
