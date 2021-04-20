e346f3c030dc75bad6ae0b58bd9b91e06553850c
SychO9 <sychocouldy@gmail.com>
2021-04-20 11:21:12 +0100

    Add settings help text

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -4,6 +4,7 @@ app.initializers.add('flarum-mentions', function() {
     .registerSetting({
       setting: 'flarum-mentions.allow_username_format',
       type: 'boolean',
-      label: app.translator.trans('flarum-mentions.admin.settings.allow_username_format')
+      label: app.translator.trans('flarum-mentions.admin.settings.allow_username_format_label'),
+      help: app.translator.trans('flarum-mentions.admin.settings.allow_username_format_text')
     });
 });
